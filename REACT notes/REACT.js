c0ee0aae8124ad5ccf8ac185beac8fc583ec4a12
Raj Rasane <rasaneraj04@gmail.com>
2024-04-24 12:59:34 +0530

    notes updated

--- REACT notes/REACT.js
+++ REACT notes/REACT.js
@@ -1,3 +1,10 @@
 // REACT
 
-// - It is a library for building UI (user interfaces). 
+// - REACT is a JS library for building UI (user interfaces). 
+// - In React, everything revolves around components.
+// - A component is a piece of the UI that has its own logic and appearance.
+// - A component is a JS function or class that returns a piece of UI.
+// - A component can be as small as a button, or as large as an entire page.
+// - React components are JS functions that return markup .
+
+//---------------------------------------------------------------------------------------------------------------------------------------
