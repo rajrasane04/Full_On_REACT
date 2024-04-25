a89bfab9dabb3aaf7b96765b4debfad4e0881c62
Raj Rasane <rasaneraj04@gmail.com>
2024-04-25 08:45:29 +0530

    new notes added

---
+++ 01]_JSX.js
@@ -0,0 +1,16 @@
+// JSX (JavaScript XML)
+
+// - JSX is a JavaScript extension that lets you write HTML-like code in JavaScript files. 
+// It's used in React to make UI structure descriptions more readable and convenient.
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
+// Key Points
+
+// - Syntax extension for JavaScript.
+// - Allows HTML-like code in JavaScript files.
+// - Enhances readability and convenience in describing UI structure.
+// - Used to build user interfaces in React applications.
+
+//---------------------------------------------------------------------------------------------------------------------------------------
+
